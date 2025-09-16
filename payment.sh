7256e7da7d17e3581e3889ba54894176946a67d0
Nagajyothipidugu <9pidugujyothi@gmail.com>
2025-09-16 19:42:26 +0530

    message

--- payment.sh
+++ payment.sh
@@ -22,8 +22,7 @@ else
    echo -e "$G You are running with root access  $N" |tee -a $LOG_FILE
 fi  
 
-echo "Please enter root password"
-read -s MYSQL_ROOT_PASSWORD
+
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
@@ -35,8 +34,8 @@ VALIDATE(){
     fi
 }
 
-dnf install maven -y  &>>$LOG_FILE
-VALIDATE $? "Installing maven " 
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+VALIDATE $? "Installing python " 
 
 id roboshop 
 if [ $? -ne 0 ]
@@ -50,46 +49,28 @@ fi
 mkdir  -p /app 
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip  &>>$LOG_FILE
-VALIDATE $? "Downloading the shipping code"
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$LOG_FILE
+VALIDATE $? "Downloading the payment code"
 
 rm -rf /app/*
 cd /app 
-unzip /tmp/shipping.zip  &>>$LOG_FILE
+unzip /tmp/payment.zip  &>>$LOG_FILE
 VALIDATE $? "Unzipping the code" 
 
 cd /app 
-mvn clean package  &>>$LOG_FILE
+pip3 install -r requirements.txt  &>>$LOG_FILE
 VALIDATE $? "installing dependecies" 
-mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
-VALIDATE $? "Renaming as shipping.jar"
 
-cp $SCRIPT_DIR/shipping.service  /etc/systemd/system/shipping.service 
-VALIDATE $? "copying shipping service" 
+
+cp $SCRIPT_DIR/payment.service  /etc/systemd/system/payment.service 
+VALIDATE $? "copying payment service" 
 
 systemctl daemon-reload &>>$LOG_FILE
 VALIDATE $? "reloading"
-systemctl enable shipping  &>>$LOG_FILE
-VALIDATE $? "Enabling shipping"
-systemctl start shipping &>>$LOG_FILE
-VALIDATE $? "Start shipping" 
-
-dnf install mysql -y  &>>$LOG_FILE
-VALIDATE $? "Installing mysql client" 
-
-mysql -h mysql.devaws46.online -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
-if [ $? -ne 0 ]
-then 
-    mysql -h mysql.devaws46.online -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h mysql.devaws46.online  -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h mysql.devaws46.online  -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
-    VALIDATE $? "Loading data into MySQL"
-else
-    echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
-fi
-
-systemctl restart shipping &>>$LOG_FILE
-VALIDATE $? "restarting shipping" 
+systemctl enable payment  &>>$LOG_FILE
+VALIDATE $? "Enabling payment"
+systemctl start payment &>>$LOG_FILE
+VALIDATE $? "Start payment" 
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME))

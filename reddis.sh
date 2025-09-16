8f4301c78f9fc6c17d746366679a167395172187
Nagajyothipidugu <9pidugujyothi@gmail.com>
2025-09-16 15:54:00 +0530

    message

--- reddis.sh
+++ reddis.sh
@@ -49,7 +49,7 @@ systemctl start redis
 VALIDATE $? "start reddis"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$( ( $START_TIME-$END_TIME ) )
+TOTAL_TIME=$(( $START_TIME-$END_TIME ))
 
 echo -e "Script execution completed successfully..$Y $TOTAL_TIME  seconds $N " | tee -a $LOG_FILE
 
